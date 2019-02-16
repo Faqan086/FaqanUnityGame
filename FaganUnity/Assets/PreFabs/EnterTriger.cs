3610803584719fa78ab36f2d71371a567313aa8f
Faqan <43637302+Faqan086@users.noreply.github.com>
2019-02-16 21:36:54 +0400

    Unity
    First Game in C Sharp

---
+++ FaganUnity/Assets/PreFabs/EnterTriger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterTriger : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject SpawnPoint;
+    public GameObject Enter_Text;
+
+    void OnTriggerEnter(Collider col)
+    {
+       // if (col.name == "EnterTrigger")
+       //     Player.transform.position = SpawnPoint.transform.position; 
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if(col.name == "EnterTrigger")
+        {
+            Enter_Text.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Player.transform.position = SpawnPoint.transform.position;
+            }
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if(col.name == "EnterTrigger")
+        {
+            Enter_Text.SetActive(false);
+        }
+    }
+}
